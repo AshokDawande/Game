a542290972350bac6ca3ac00b0733f267952cdc6
WebDeveloperAD <152083232+AshokDawande@users.noreply.github.com>
2024-05-06 19:25:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ window.onload = function(){
     const up = document.getElementById("u");
     const down = document.getElementById("d");
     
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
     
     const speed = 20;
     
